cbdab3e8c9f1f0980fbd56435907f5585d01de2a
wsm-web <123707837+wsm-web@users.noreply.github.com>
2023-03-27 19:49:14 -0600

    Add files via upload

--- FitLife/producto/ensalada de frutas/script.js
+++ FitLife/producto/ensalada de frutas/script.js
@@ -4,6 +4,6 @@ const enviarFormulario =() => {
         let apellido = document.getElementById('apellidos').value;
         let mensaje = document.getElementById('mensaje').value;
         let numero= 50587427199;
-var win= window.open(`https://wa.me/${numero}?text=Hola%20vengo%20de%20*FitLife*!%0D%0A%0D%0A*Pedido:*%0D%0A-%20Ensalada%20Cesar%20de%20la%20casa%20(C$%20220)%0D%0A%0D%0A*Nombre:*%0D%0A-%20${nombre}%20${apellido}.%0D%0A%0D%0A*Dirección de entrega:*%0D%0A-%20${mensaje}%0D%0A%0D%0A*Envía este mensaje para crear tu pedido.*`,'_blank');       
+var win= window.open(`https://wa.me/${numero}?text=Hola%20vengo%20de%20*FitLife*!%0D%0A%0D%0A*Pedido:*%0D%0A-%20Ensalada%20de%20frutas%20(C$%2090)%0D%0A%0D%0A*Nombre:*%0D%0A-%20${nombre}%20${apellido}.%0D%0A%0D%0A*Dirección de entrega:*%0D%0A-%20${mensaje}%0D%0A%0D%0A*Envía este mensaje para crear tu pedido.*`,'_blank');       
 }
 evento.addEventListener('click', enviarFormulario)
--- FitLife/producto/ensalada de papas/script.js
+++ FitLife/producto/ensalada de papas/script.js
@@ -3,7 +3,7 @@ const enviarFormulario =() => {
         let nombre = document.getElementById('nombres').value;
         let apellido = document.getElementById('apellidos').value;
         let mensaje = document.getElementById('mensaje').value;
-        let numero= 50577964747;
-var win= window.open(`https://wa.me/${numero}?text=Hola%20vengo%20de%20*FitLife*!%0D%0A%0D%0A*Pedido:*%0D%0A-%20Nombre%20del%20producto%20(Precio)%0D%0A%0D%0A*Nombre:*%0D%0A-%20${nombre}%20${apellido}.%0D%0A%0D%0A*Dirección de entrega:*%0D%0A-%20${mensaje}%0D%0A%0D%0A*Envía este mensaje para crear tu pedido.*`,'_blank');       
+        let numero= 50587427199;
+var win= window.open(`https://wa.me/${numero}?text=Hola%20vengo%20de%20*FitLife*!%0D%0A%0D%0A*Pedido:*%0D%0A-%20Ensalada%20de%20papas%20(C$%20220)%0D%0A%0D%0A*Nombre:*%0D%0A-%20${nombre}%20${apellido}.%0D%0A%0D%0A*Dirección de entrega:*%0D%0A-%20${mensaje}%0D%0A%0D%0A*Envía este mensaje para crear tu pedido.*`,'_blank');       
 }
 evento.addEventListener('click', enviarFormulario)
